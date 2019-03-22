23890abe915ad41fea79f6a041fe4171c1539ad2
jeanqussa <jean.qussa@gmail.com>
2019-03-22 17:42:01 +0200

    Parse command line arguments

--- NameList/Program.cs
+++ NameList/Program.cs
@@ -24,13 +24,42 @@ namespace Homework.NameList
             return pairsArray;
         }
 
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var dict = CSVReader.ReadPairsFromCSVFile("./names.csv");
-            var sorted = SortPairs(ref dict, false);
+            if (args.Length == 0) {
+                Console.WriteLine("Please specify a file name.");
+                Console.WriteLine("Usage: NameList [-d] <filename>");
+                Console.WriteLine("");
+                Console.WriteLine("Options:");
+                Console.WriteLine("  -d (optional): sort in descending order");
+                Console.WriteLine("  <filename>: path to CSV file");
+                Console.WriteLine("");
+                Console.WriteLine("Examples:");
+                Console.WriteLine("  NameList names.csv");
+                Console.WriteLine("  NameList -d names.csv");
+                return 1;
+            }
+
+            bool descending = false;
+            if (args[0] == "-d") {
+                descending = true;
+            }
+            string filename = args[args.Length - 1];
+
+            Dictionary<int, string> names;
+            try {
+                names = CSVReader.ReadPairsFromCSVFile(filename);
+            } catch (Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+                return 1;
+            }
+
+            var sorted = SortPairs(ref names, descending);
             foreach (var entry in sorted) {
                 Console.WriteLine("" + entry.Key + " " + entry.Value);
             }
+
+            return 0;
         }
     }
 }
