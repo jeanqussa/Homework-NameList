825afe43acffe1bde265072642a4c7a90bf76da4
jeanqussa <jean.qussa@gmail.com>
2019-03-22 16:49:56 +0200

    Add sorting methods

--- NameList/Program.cs
+++ NameList/Program.cs
@@ -1,32 +1,33 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
 using System.Collections.Generic;
+using Homework.NameList;
 
 namespace Homework.NameList
 {
     class Program
     {
         /// <summary>
+        /// <para>
         /// Read a list of name/ID pairs from a comma-separated text file whose path is passed in
         /// in the <c>filename</a> parameter.
-        /// 
+        /// </para>
+        /// <para>
         /// The file must contain one entry on each line in the following format:
         /// 
         /// <code>
         /// First Name;1<br>
         /// Second Name;5
         /// </code>
+        /// </para>
         /// </summary>
         /// <param name="filename">Path and name of CSV file.</param>
+        /// <returns>A <c>Dictionary&lt;int, string&gt;</c> with values read from the file.</returns>
         /// <exception cref="System.IO.FileNotFoundException">Thrown when <c>filename</c> is not found.</exception>
         /// <exception cref="System.IO.DirectoryNotFoundException">Thrown when part of the path in <c>filename</c> is not found.</exception>
         /// <exception cref="System.IO.IOException">Thrown when invalid <c>filename</c> is passed.</exception>
         /// <exception cref="System.IO.FormatException">Thrown when file contains an invalid format.</exception>
-        /// <example>
-        /// <code>
-        /// var pairs = ReadPairsFromCSVFile("pairs.csv");
-        /// </code>
-        /// </example>
         public static Dictionary<int, string> ReadPairsFromCSVFile(string filename) {
             var dict = new Dictionary<int, string>();
 
@@ -44,7 +45,7 @@ namespace Homework.NameList
                         throw new FormatException("Invalid ID number: " + values[1]);
                     }
 
-                    if (dict.ContainsKey[key]) {
+                    if (dict.ContainsKey(key)) {
                         throw new FormatException("Duplicate key: " + key);
                     }
                     dict.Add(key, values[0]);
@@ -54,12 +55,28 @@ namespace Homework.NameList
             return dict;
         }
 
-
+        /// <summary>
+        /// Converts a <c>Dictionary</c> into a <c>List</c> of <c>KeyValuePair</c>s and sorts it in
+        /// ascending or descending order based on the dictionary's key.
+        /// </summary>
+        /// <param name="pairs"><c>Dictionary&lt;int, string&gt;</c> to sort.</param>
+        /// <param name="descending">Whether to sort in descending order. Default = false.</param>
+        /// <returns>A <c>List&lt;KeyValuePair&lt;int, string&gt;&gt;</c> containing all values 
+        /// in the dictionary but in sorted order.</returns>
+        public static List<KeyValuePair<int, string>> SortPairs(ref Dictionary<int, string> pairs, bool descending = false) {
+            var pairsArray = new List<KeyValuePair<int, string>>();
+            foreach (var entry in pairs) {
+                pairsArray.Add(entry);
+            }
+            QuickSort.Sort(ref pairsArray, (a, b) => (a.Key > b.Key) == descending && a.Key != b.Key, 0, pairsArray.Count-1);
+            return pairsArray;
+        }
 
         static void Main(string[] args)
         {
             var dict = ReadPairsFromCSVFile("./names.csv");
-            foreach (var entry in dict) {
+            var sorted = SortPairs(ref dict, false);
+            foreach (var entry in sorted) {
                 Console.WriteLine("" + entry.Key + " " + entry.Value);
             }
         }
---
+++ NameList/QuickSort.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Homework.NameList
+{
+    public class QuickSort
+    {
+        /// <summary>
+        /// Swaps two elements in a <c>List</c>.
+        /// </summary>
+        /// <param name="a">Index of first element.</param>
+        /// <param name="b">Index of second element.</param>
+        public static void Swap<T>(ref List<T> elements, int a, int b) {
+            var temp = elements[a];
+            elements[a] = elements[b];
+            elements[b] = temp;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Partitions a list using the <a href="https://en.wikipedia.org/wiki/Quicksort#Hoare_partition_scheme">Hoare partition scheme</a>.
+        /// </para>
+        /// <para>
+        /// Picks a value from <c>elements</c> (called a pivot) and partitions the list such that all
+        /// elements with values less than the pivot are placed to its left, and all values greater
+        /// are placed to its right.
+        /// </para>
+        /// </summary>
+        /// <param name="elements">List of elements to partition.</param>
+        /// <param name="shouldBeBefore">A function which, when given two values, returns whether
+        /// the first value should appear before the second value in the sorted list.</param>
+        /// <param name="left">Index of element in array at which the partition begins.</param>
+        /// <param name="right">Index of element in array at which the partition ends.</param>
+        /// <returns>The new index of the pivot.</returns>
+        public static int Partition<T>(ref List<T> elements, Func<T, T, bool> shouldBeBefore, int left, int right) {
+            var pivot = elements[(left + right) / 2];
+
+            int i = left - 1;
+            int j = right + 1;
+            
+            while (true) {
+                do {
+                    i += 1;
+                } while (shouldBeBefore(elements[i], pivot) && !elements[i].Equals(pivot));
+
+                do {
+                    j -= 1;
+                } while (shouldBeBefore(pivot, elements[j]) && !elements[j].Equals(pivot));
+
+                if (i >= j) {
+                    return j;
+                }
+
+                Swap(ref elements, i, j);
+            }
+        }
+
+        /// <summary>
+        /// <para>
+        /// Sorts a list, in-place, using the quicksort algorithm. Since the sorting is done in-place,
+        /// it is an unstable sorting algorithm.
+        /// </para>
+        /// <para>
+        /// See <a href="https://en.wikipedia.org/wiki/Quicksort">Quicksort</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="elements">List of elements to partition.</param>
+        /// <param name="shouldBeBefore">A function which, when given two values, returns whether
+        /// the first value should appear before the second value in the sorted list.</param>
+        /// <param name="left">Index of element in array at which the partition begins.</param>
+        /// <param name="right">Index of element in array at which the partition ends.</param>
+        /// <returns>The new index of the pivot.</returns>
+        public static void Sort<T>(ref List<T> elements, Func<T, T, bool> shouldBeBefore, int left, int right) {
+            if (left >= right) return;
+            int pivot = Partition(ref elements, shouldBeBefore, left, right);
+            Sort(ref elements, shouldBeBefore, left, pivot);
+            Sort(ref elements, shouldBeBefore, pivot + 1, right);
+        }
+    }
+}
